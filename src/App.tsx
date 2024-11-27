337a1a3e3c7161d05124bf1a1526f3b628818f66
Asish-15 <klucse2000031408@gmail.com>
2024-11-28 03:17:15 +0530

    done with buttons need to add wallet

--- src/App.tsx
+++ src/App.tsx
@@ -9,40 +9,50 @@ const GameWrapper1 = () => {
 
 const GameWrapper2 = () => {
   return(
-    <div className=" bg-blue-200 h-screen flex justify-center align-middle"> 
+    <div className=" h-screen flex justify-center align-middle"> 
       <div className="my-auto w-4/6">
-        <div className="bg-red-100 flex justify-center" >
+        <div className=" flex justify-center" >
           <button className="bg-green-400 text-white px-6 py-2 hover:bg-green-200 rounded-3xl shadow-md my-4">Start Game</button>
         </div>
         
         
         
-        <div className="bg-pink-100 flex justify-center gap-2">
-          <div className="bg-yellow-300 w-1/2">
-          <div className="bg-white  px-3 py-1  rounded-xl shadow-md my-4">
-            <div>
-              <div className="bg-gray-400  px-4 py-2 text-center text-x1 font-bold rounded-xl  my-4">Select your move</div>
-            </div>
-            
-            
-            
-            
-            <div className="flex gap-5">
-              <div className="gap-3"> 
+        <div className="flex justify-center gap-2">
+          <div className=" w-1/2 px-4 "> 
+            <div className="bg-white-500  px-6 py-2  rounded-xl shadow-md my-4">
+              <div>
+                <div className="bg-gray-600  px-4 py-2 text-center text-x1 font-bold rounded-xl  my-4">Select your move</div>
+              </div>
+              
+              <div className="flex justify-between"> 
                 <button className="bg-red-500 text-white hover:bg-green-200 text-2xl px-4 py-2 rounded-xl shadow-md my-4" >Clear</button>
-                <button className="bg-orange-500 text-white text-2xl px-4 py-2 rounded-xl shadow-md my-4" >Rock</button>
-                <button className="bg-orange-500 text-white text-2xl px-4 py-2 rounded-xl shadow-md my-4" >Paper</button>
-                <button className="bg-orange-500 text-white text-2xl px-4 py-2 rounded-xl shadow-md my-4" >Scissors</button> 
+                <button className="bg-orange-500 text-white text-2xl px-8 py-4 rounded-xl shadow-md my-4" >Rock</button>
+                <button className="bg-orange-500 text-white text-2xl px-8 py-4 rounded-xl shadow-md my-4" >Paper</button>
+                <button className="bg-orange-500 text-white text-2xl px-8 py-4 rounded-xl shadow-md my-4" >Scissors</button> 
               </div>
             </div>
           </div>
+
+          <div className=" w-1/2 px-4">
+            <div className="bg-white-500  px-6 py-2  rounded-xl shadow-md my-4">
+              <div>
+                <div className="bg-gray-600  px-4 py-2 text-center text-x1 font-bold rounded-xl  my-4">Computer move</div>
+              </div>
+              
+              <div className="flex justify-between"> 
+                <button className="bg-orange-500 text-white text-2xl px-8 py-4 rounded-xl shadow-md my-4" >Rock</button>
+                <button className="bg-orange-500 text-white text-2xl px-8 py-4 rounded-xl shadow-md my-4" >Paper</button>
+                <button className="bg-orange-500 text-white text-2xl px-8 py-4 rounded-xl shadow-md my-4" >Scissors</button> 
+              </div>
+            </div>
           </div>
-          <div className="bg-blue-300 w-1/2">Div2</div>
+          
+          
         </div>
         
         
         
-        <div className="bg-orange-100 flex justify-center">
+        <div className=" flex justify-center">
           <div className="bg-green-600 text-3xl text-bold px-44 py-4  rounded-xl  my-4">Game Result</div>
         </div>
       </div>
